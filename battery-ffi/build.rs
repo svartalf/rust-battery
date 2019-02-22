7f895ed9a18a8f044b798af27239d0f8f4a64c6c
svartalf <self@svartalf.info>
2019-02-22 23:45:41 +0300

    Disabling cbindgen generation while building documentation at docs.rs

--- battery-ffi/build.rs
+++ battery-ffi/build.rs
@@ -1,13 +1,22 @@
-use std::env;
-use std::path::PathBuf;
+#[cfg(feature = "cbindgen")]
+fn build_header() {
+    use std::env;
+    use std::path::PathBuf;
 
-fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let crate_dir = env::var("CARGO_MANIFEST_DIR")
+        .expect("CARGO_MANIFEST_DIR env var is not defined");
+    let out_dir = PathBuf::from(env::var("OUT_DIR")
+        .expect("OUT_DIR env var is not defined"));
 
-    let config = cbindgen::Config::from_file("cbindgen.toml").unwrap();
+    let config = cbindgen::Config::from_file("cbindgen.toml")
+        .expect("Unable to find cbindgen.toml configuration file");
 
     cbindgen::generate_with_config(&crate_dir, config)
         .unwrap()
         .write_to_file(out_dir.join("battery_ffi.h"));
+}
+
+fn main() {
+    #[cfg(feature = "cbindgen")]
+    build_header()
 }
